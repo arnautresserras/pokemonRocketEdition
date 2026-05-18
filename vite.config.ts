b1054aa7afa756a83d666d445aee4b5213521a54
arnautresserras <arnau.tresserras@htsi.url.edu>
2026-05-18 19:39:24 +0200

    Update gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // CI env var is set automatically by GitHub Actions
-  base: process.env.CI ? '/pokemonRocketEdition/' : '/',
+  base: '/pokemonRocketEdition/',
 })
